43a2dee7b3ef12413aa310d2a9f2453dcc9876fb
SithumDilanga <sithum.9dilanga@gmail.com>
2021-06-13 18:19:26 +0530

    API fecth and basic UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import { render } from '@testing-library/react';
+import backgroundImage from './images/card-img.jpg';
 
-function App() {
+class App extends React.Component{
 
-  const elements = [];
-
-  for(let i = 0; i < 20; i++){
-    elements.push(
-      <div className = "item">item {i}</div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      elements: []
+    }
   }
 
-  return (
-    <div className = "grid">
-      {elements}
-    </div>
-  );
+  componentDidMount() {
+    fetch('https://type.fit/api/quotes')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          elements: data
+        });
+      })
+  }
+
+    render() {
+      return (
+        <div className = "grid">
+          {this.state.elements.length && this.state.elements.map((item, index) => {
+            return (
+              <div className = "card" style = {{backgroundImage: `url(${backgroundImage})`}}> 
+              {/* <img className = "card" src = "/card-img.jpg"/> */}
+                <div className = "quoteText" key={index}>{item.text}</div><br></br>
+                <div className = "quoteAuthor">- {item.author}</div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+    
+
+  
 }
 
 export default App;
+
+
+  // const [userData, setUserData] = useState({});
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // async function fetchData() {
+  //   const response = await fetch('https://type.fit/api/quotes');
+  //   const jsonData = await response.json();
+  //   setUserData(jsonData)
+  // }
+
+
+  
+  
+  // for(let i = 0; i < 20; i++){
+  //   elements.push(
+  //     <div className = "item">item {0}</div>
+  //     );
+  //   }
